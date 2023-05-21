5ad8ae2688979d8d22f08cfcb39b3748d270798a
Karen Alves <karen.alves@ccc.ufcg.edu.br>
2023-05-21 02:55:50 -0300

    feat: reduce size of buttons and change border

--- components/ui/Highlights.tsx
+++ components/ui/Highlights.tsx
@@ -21,16 +21,16 @@ function Highlights({ highlights = [], title }: Props) {
         <span class="font-medium text-2xl">{title}</span>
       </h2>
 
-      <Slider class="carousel carousel-center sm:carousel-end gap-6">
+      <Slider class="carousel carousel-center justify-center sm:carousel-end gap-6">
         {highlights.map(({ href, src, alt, label }, index) => (
           <Slider.Item
             index={index}
-            class="carousel-item first:ml-6 sm:first:ml-0 last:mr-6 sm:last:mr-0 min-w-[190px]"
+            class="carousel-item"
           >
             <a href={href} class="card card-compact bg-base-100">
               <figure>
                 <Image
-                  class="rounded-[40px]"
+                  class="rounded-md w-10 h-10"
                   src={src}
                   alt={alt}
                   width={190}
@@ -38,7 +38,7 @@ function Highlights({ highlights = [], title }: Props) {
                 />
               </figure>
               <div class="card-body items-center">
-                <h2 class="card-title text-base font-medium">{label}</h2>
+                <span class="card-title text-slate-500 font-normal">{label}</span>
               </div>
             </a>
           </Slider.Item>
